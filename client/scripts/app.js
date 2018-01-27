e08d0e726e8a466bb38226449c37167ff91e0753
sr5x4fc3s <alau1991@hotmail.com>
2018-01-26 19:52:39 -0800

    changed a bunch of stuff and added new init.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,34 @@
 // YOUR CODE HERE:
+// var request = (type, message) => {
+//   $.ajax({
+//     // This is the url you should use to communicate with the parse API server.
+//     // url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+//     url: this.server,
+//     type: type,
+//     data: message,
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('chatterbox: Message sent');
+//     },
+//     error: function (data) {
+//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.error('chatterbox: Failed to send message', data);
+//     }
+//   });
+// };
+var myVar;
+
 var app = {
+  // server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
   init: () => {
     app.handleUsernameClick();
     app.handleSubmit();
-
   },
   send: (message) =>{
+    // request('POST', message);
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: message,
       contentType: 'application/json',
@@ -21,15 +41,18 @@ var app = {
       }
     });
   },
-  fetch: () => {
+  fetch: (message) => {
+    // request('GET', message);
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: undefined,
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
       data: message,
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Message received', data);
+        // return this.data;
+        myVar = data;
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -49,14 +72,16 @@ var app = {
     $('#roomSelect').html(room);
   },
   handleUsernameClick: () => { 
-    $('.username').on('click', (event) => {
+    // $('.username').on('click', (event) => {
       
-    });
+    // });
+    return 'hello';
   },
   handleSubmit: () => {
-    $('.submit').on('click', (event) => {
+    // $('.submit').on('click', (event) => {
       
-    });
+    // });
+    return 'goodbye';
   }
   
 };
---
+++ client/scripts/init.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+  
+//   $('.submit').on('click', function(event) {
+
+//     $('body').append(.$node);  
+//   });
+
+//   $('.username').on('click', function(event) {
+
+//     $('body').append(.$node);  
+//   });
+// });
