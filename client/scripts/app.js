1f892f84c6faeebd67f8b7cc68b2e85736043b34
sr5x4fc3s <alau1991@hotmail.com>
2018-01-27 16:49:49 -0800

    Completed requierments for Sending and Fetching messages to post. Still working on socializing.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -16,6 +16,7 @@
 //     }
 //   });
 // };
+
 var myVar;
 //= [{createdAt: "2017-12-08T20:55:12.526Z",
 //   objectId: "hEG6XDGsEE",
@@ -24,7 +25,21 @@ var myVar;
 //   username: "cat"}]; //get data
 
 
-
+var filter = (array) => { // myVar.results
+  var filteredArr = [];
+  var arr = [];
+  array.forEach(x => {
+    if (x.roomname) {
+      arr.push(x.roomname);
+    } 
+  });
+  for (var i = 0; i < arr.length; i++) {
+    if (!filteredArr.includes(arr[i])) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
+};
 
 //example data:
 
@@ -36,19 +51,39 @@ var myVar;
 // username: "cat"
 //},...];
 
+var userMessage = ''; // this will hold text from this.handleSubmit
+var userRoom = '';
+
+
 var app = {
   // server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
   init: () => {
-    app.handleUsernameClick();
-    app.handleSubmit();
+    app.fetch();
+    app.renderMessage();
+    app.renderRoom();
+    // app.handleUsernameClick();
+    // app.handleUsernameClick();
+    // app.handleSubmit();
   },
   send: (message) =>{
     // request('POST', message);
+
+    var createObject = (room, text) => {
+      var getUserIndex = window.location.search.indexOf('=');
+      var getUser = window.location.search.slice(getUserIndex + 1);
+      var objTemplate = {
+        roomname: room,
+        text: text,
+        username: getUser
+      };
+      return objTemplate;
+    };
+    var message = createObject(userRoom, userMessage);
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
-      data: message,
+      data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
@@ -83,59 +118,68 @@ var app = {
   clearMessages: () => {
     $('#chats').empty(); 
   },
-  renderMessage: (newMessage) => {
-    var message = `<blink>${newMessage.text}</blink>`;
-    $('#chats').html(message);
+  renderMessage: () => {
+    var index = myVar.results.length - 1;
+    while (index >= 0) {
+      if (myVar.results[index].text !== undefined) {
+        var messages = _.escape(myVar.results[index].text);
+        var name = _.escape(myVar.results[index].username); 
+        var username = (`<a href="" class= "${name}">${name}</a>`);
+        var $post = $('<div></div>');
+        // figure out how to decode here!
+        $post.html(`${username}: ${messages}`);
+        $post.appendTo('#chats');
+      }
+      index -= 1;
+    }
   },
-  renderRoom: (roomName) => {
-    var room = `<option value=${roomName.roomname}>${roomName.roomname}</option>`;
-    $('#roomSelect').html(room);
+  renderRoom: () => {
+    var filteredRooms = filter(myVar.results); // will be an array 
+    var index = filteredRooms.length - 1;
+    while (index >= 0) {
+      var room = _.escape(filteredRooms[index]);
+      var $room = $(`<option value="${room}"></option>`);
+      // figure out how to decode here!
+      $room.html(`${room}`);
+      $room.appendTo('#roomSelect');
+      index -= 1;
+    }  
   },
   handleUsernameClick: () => { 
     // $('.username').on('click', (event) => {
       
     // });
-    return 'hello';
+    alert('hello');
   },
   handleSubmit: () => {
-    // $('.submit').on('click', (event) => {
-      
-    // });
-    return 'goodbye';
+    $('.submit').on('click', (event) => {
+      userMessage = $('#currentMessage').val();
+      userRoom = $('#roomSelect').val();
+      app.send();
+    });
   },
-  postMesage: () => {
-    var index = 20;
-    while (index >= 0) {
-      if (myVar.results[index].text !== undefined) {
-        var messages = encodeURI(myVar.results[index].text);
-        var name = JSON.stringify(myVar.results[index].username); 
-        var $post = $('<blink></blink>');
-        $post.html(`${name}: ${messages}`);
-        $post.appendTo('#chats');
-      }
-      index -= 1;
-    }
+  handleRoomSubmit: () => {
+    $('.roomSubmit').on('click', (event) => {
+      var userRoom = $('#newRoom').val();
+      var room = _.escape(userRoom);
+      var $room = $(`<option value="${room}"></option>`);
+      // figure out how to decode here!
+      $room.html(`${room}`);
+      $room.appendTo('#roomSelect');
+    });
   }
-  
 };
 
 // set interval to fetch messages => save old data somewhere and append new instances to the body 
-/*setInterval(() => {
-  app.fetch();
-  app.postMesage();
-}, 5000);*/
+app.init();
 
 
 $(document).ready(function() {
-  
-    // app.postMesage();
-  // $('.submit').on('click', function(event) {
 
-  //   $('body').append($node);  
-  // });
+  app.handleSubmit();
+  app.handleRoomSubmit();
+  setInterval(() => {
+    app.init();
+  }, 15000);
 
-  // $('.username').on('click', function(event) {
-
-  //   $('body').append($node);  
-  // });
 });
